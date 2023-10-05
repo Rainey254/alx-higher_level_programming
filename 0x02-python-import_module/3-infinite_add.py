0608715299a66bb84e01db748c3975fcdea2c487
rainey254 <reinykenya@gmail.com>
2023-10-05 08:04:19 -0700

    anything

--- 0x02-python-import_module/3-infinite_add.py
+++ 0x02-python-import_module/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))
 
-import sys
 
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
