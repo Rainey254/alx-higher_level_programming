607d5a107212fbfb926dab0c8a612b55516b448f
rainey254 <reinykenya@gmail.com>
2024-01-08 10:14:47 -0800

    anything

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-
-const cFun = 'C is fun';
-const pyCool = 'Python is cool';
-const jAmaze = 'JavaScript is amazing';
-
-console.log(cFun);
-console.log(pyCool);
-console.log(jAmaze);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
